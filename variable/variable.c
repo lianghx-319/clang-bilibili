b006bb4a8fbed14309fbe07cf020cf65bec42f94
Hanxx <xsytby1112@gmail.com>
2021-10-09 12:47:43 +0800

    feat: variable

---
+++ variable/variable.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("%d", sum);
+	return 0;
+}
