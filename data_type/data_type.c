56da5f76a21e1a882dd929d0869a30122ee85d02
Hanxx <xsytby1112@gmail.com>
2021-10-09 12:13:02 +0800

    feat: data type

---
+++ data_type/data_type.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	/* code */
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(short));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(long));
+	printf("%lu\n", sizeof(long long));
+	printf("%lu\n", sizeof(float));
+	printf("%lu\n", sizeof(double));
+	return 0;
+}
