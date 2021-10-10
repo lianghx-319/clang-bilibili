0d484857726d4ebd9fee72e4b8b5751ab4734037
Hanxx <xsytby1112@gmail.com>
2021-10-10 17:25:27 +0800

    feat: struct

---
+++ struct/student.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct Student
+{
+	char name[20];
+	int age;
+	double score;
+};
+
+int main(int argc, char const *argv[])
+{
+	struct Student s = {"a", 20, 20.4};
+	printf("%s %d %lf\n", s.name, s.age, s.score);
+
+	struct Student* ps = &s;
+	printf("%s %d %lf\n", ps->name, ps->age, ps->score);
+	return 0;
+}
+
