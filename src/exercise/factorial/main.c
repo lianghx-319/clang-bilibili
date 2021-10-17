03990a3fb782173767b4b7227b806a88c83a08db
Hanxx <xsytby1112@gmail.com>
2021-10-17 10:19:48 +0800

    feat: exercise sum factorial

--- src/exercise/factorial/main.c
+++ src/exercise/factorial/main.c
@@ -9,10 +9,24 @@ int factorial(int n) {
     return n * factorial(n - 1);
 }
 
+int sum_factorial(int n) {
+    if (n <= 1) return 1;
+    int sum = 0;
+    int factorial = 1;
+    for (int i = 1; i <= n; i++) {
+        factorial *= i;
+        sum += factorial;
+    }
+
+    return sum;
+}
+
 int main() {
     int n = 0;
     scanf("%d", &n);
     int ret = factorial(n);
-    printf("factorial of %d is %d", n, ret);
+    int sum = sum_factorial(n);
+    printf("factorial of %d is %d\n", n, ret);
+    printf("sum factorial from %d to 1 is %d", n, sum);
     return 0;
 }
